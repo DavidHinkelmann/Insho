d6eeea6b139620ac8a4297dbfacb126e3c5a9401
Niklas Deimer <n.deimer@optadata-gruppe.de>
2025-09-09 12:47:44 +0200

    impleting alembic as migration tool. Watch Todos! Can't fix them from home

--- Core/app/main.py
+++ Core/app/main.py
@@ -1,14 +1,20 @@
 from fastapi import FastAPI
+from flask import Flask
 
 from .core.config import get_settings
 from .api.router import api_router
-
+from .migration import migration
 
 settings = get_settings()
 app = FastAPI(title=settings.PROJECT_NAME, version=settings.VERSION)
 
 # Mount versioned API
 app.include_router(api_router, prefix="/api/v1")
+
+application = Flask(__name__)
+db = SQLAlchemy()
+db.init_app(application)
+migration.migrate(application)
 
 
 @app.get("/")
